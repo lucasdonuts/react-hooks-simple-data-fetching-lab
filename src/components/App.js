e5117db9896d5cec2b74653ca06b14351e230908
Lucas Duncan <lucasdonuts@gmail.com>
2022-07-22 14:29:13 -0500

    commit tree fffe9602377021a4a60e10317806d5e025778e11

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,27 @@
-// create your App component here
+import { useEffect, useState, React } from 'react';
+
+const App = () => {
+  const [isLoaded, setIsLoaded] = useState(false)
+  const [imageSrc, setImageSrc] = useState('https://upload.wikimedia.org/wikipedia/commons/6/65/No-Image-Placeholder.svg')
+  
+  useEffect( () => {
+    fetch('https://dog.ceo/api/breeds/image/random')
+      .then( res => res.json() )
+      .then( data => {
+        setImageSrc(data.message);
+        setIsLoaded(loaded => !loaded);
+      })
+  }, []);
+
+  if(!isLoaded) {
+    return <p>Loading...</p>
+  }
+
+  return(
+    <div> 
+      <img alt='A Random Dog' src={ imageSrc } />
+    </div>
+  )
+};
+
+export default App;
